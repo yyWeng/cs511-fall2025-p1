8f06af68c25da744f6aacb1417612f4f9ef57265
Nancyzxy <11912039@mail.sustech.edu.cn>
2025-09-15 13:40:17 -0500

    update start-all.sh to use "docker compose" instead of "docker-compose".

--- start-all.sh
+++ start-all.sh
@@ -2,4 +2,4 @@
 docker build -t cs511p1-common -f cs511p1-common.Dockerfile . && \
     docker build -t cs511p1-main -f cs511p1-main.Dockerfile . && \
     docker build -t cs511p1-worker -f cs511p1-worker.Dockerfile . && \
-    docker-compose -f cs511p1-compose.yaml up
+    docker compose -f cs511p1-compose.yaml up
